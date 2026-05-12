b8c83d9ec351eb090e3ea54f728d0aff6e6ee70d
cristianoexcia <cristiano.excia@gmail.com>
2026-05-12 09:34:04 -0300

    upload audio

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import OpenAI from 'openai';
+import fs from 'fs';
 
 import userRoutes from './routes/users.js'
 
@@ -10,30 +12,9 @@ import multer from 'multer';
 import path from 'path';
 const app = express()
 
-/*const storage = multer.diskStorage({
-
-  destination: function (
-    req,
-    file,
-    cb
-  ) {
-
-    cb(null, 'uploads/');
-  },
-
-  filename: function (
-    req,
-    file,
-    cb
-  ) {
-
-    const unique =
-      Date.now() +
-      path.extname(file.originalname);
-
-    cb(null, unique);
-  }
-});*/
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
 
 const storage = multer.diskStorage({
 
@@ -83,13 +64,33 @@ app.post('/voice/upload',
 
       console.log(req.file);
 
-      return res.json({
+      /*return res.json({
 
         success: true,
 
         file: req.file.filename,
 
         path: req.file.path
+      });*/
+      const transcription =
+      await openai.audio.transcriptions.create({
+
+        file: fs.createReadStream(
+          req.file.path
+        ),
+
+        model: 'whisper-1'
+      });
+
+      console.log(
+        transcription.text
+      );
+
+      return res.json({
+
+        success: true,
+
+        text: transcription.text
       });
 
     } catch (err) {
